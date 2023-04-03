56d78a04e7b90f1043db03ca2277f6104d5e16cf
trunnghieu01 <work.trunghieu.0107@gmail.com>
2023-04-03 19:41:48 +0700

    FEAT: Init repo layer

--- src/main/java/com/thlogistic/transportation/transportation/adapters/repositories/TransportationRepositoryImpl.java
+++ src/main/java/com/thlogistic/transportation/transportation/adapters/repositories/TransportationRepositoryImpl.java
@@ -1,13 +1,29 @@
 package com.thlogistic.transportation.transportation.adapters.repositories;
 
 import com.thlogistic.transportation.transportation.core.ports.TransportationRepository;
+import com.thlogistic.transportation.transportation.entities.TransportationEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class TransportationRepositoryImpl implements TransportationRepository {
     private final MongoTransportationRepository repository;
 
+    @Override
+    public String insert(TransportationEntity transportation) {
+        return repository.insert(transportation).getId();
+    }
 
+    @Override
+    public String save(TransportationEntity transportation) {
+        return repository.save(transportation).getId();
+    }
+
+    @Override
+    public Optional<TransportationEntity> findById(String id) {
+        return repository.findById(id);
+    }
 }
