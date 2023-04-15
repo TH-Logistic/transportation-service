373c327c0593779765974a87f37e067badb4d2e7
trunnghieu01 <work.trunghieu.0107@gmail.com>
2023-04-15 23:40:55 +0700

    FEAT: Snake case and mongo config

--- src/main/java/com/thlogistic/transportation/adapters/dtos/UpdateTransportationRequest.java
+++ src/main/java/com/thlogistic/transportation/adapters/dtos/UpdateTransportationRequest.java
@@ -1,5 +1,7 @@
 package com.thlogistic.transportation.adapters.dtos;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UpdateTransportationRequest {
     @NotBlank(message = "Invalid license plate")
     String licensePlate;
